21a4859f8ef36e7de7d80feb4eb6964cc5aad93f
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2022-10-14 16:26:56 -0400

    updated the about section

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About(){
     return(
         <div>
         <h5 className="title">About</h5>
-        <p>I am a self taught frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+        <p>I am a self taught frontend developer with a particular interest in UX/UI designing. I wish to become a full stack developer and am always looking for new things to learn.</p>
         </div>
     )
 }
