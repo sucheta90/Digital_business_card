94ddde1c3e1317e95f408b3f4aa4de367653ee7f
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-06-05 18:33:15 -0400

    changed profile picture and changed code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import FacebookIcon from './images/Icons/square-facebook-brands.svg';
-import Instagram from './images/Icons/square-instagram-brands.svg'
-import Twitter from './images/Icons/square-twitter-brands.svg'
-import Github from './images/Icons/square-github-brands.svg'
-export default function Footer(){
-    return(
-        <div className="footer">
-            <span><a href="https://twitter.com/Sucheta_Mu" target="_blank"><img src={Twitter}/></a></span>
-            <span><a href="https://www.facebook.com/sucheta.mukherjee.3/" target="_blank"><img src={FacebookIcon}/></a></span>
-            <span><a href="https://www.instagram.com/suchetamukherjee/" target="_blank"><img src={Instagram}/></a></span>
-            <span><a href="https://github.com/sucheta90" target="_blank"><img src={Github}/></a></span>
-        </div>
-    )
+import React from "react";
+import FacebookIcon from "./images/Icons/square-facebook-brands.svg";
+import Instagram from "./images/Icons/square-instagram-brands.svg";
+import Twitter from "./images/Icons/square-twitter-brands.svg";
+import Github from "./images/Icons/square-github-brands.svg";
+export default function Footer() {
+  return (
+    <div className="footer">
+      <span>
+        <a
+          href="https://twitter.com/Sucheta_Mu"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={Twitter} alt="Twitter Logo" />
+        </a>
+      </span>
+      <span>
+        <a
+          href="https://www.facebook.com/sucheta.mukherjee.3/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={FacebookIcon} alt="Facebook logo" />
+        </a>
+      </span>
+      <span>
+        <a
+          href="https://www.instagram.com/suchetamukherjee/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={Instagram} alt="Instagram logo" />
+        </a>
+      </span>
+      <span>
+        <a href="https://github.com/sucheta90" target="_blank" rel="noreferrer">
+          <img src={Github} alt="github logo" />
+        </a>
+      </span>
+    </div>
+  );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import MyImage from './images/IMG-1.png'; 
-export default function Nav(){
-    return (
-        <div>
-            <img src={MyImage} className="profile-picture"/>
-            <h4 className="subject-name">Sucheta Mukherjee</h4>
-            <h6 className="subject-title">Frontend Developer</h6>
-            <p className="subject-website">mukherjeesucheta.website</p>
-        </div>
-    )
+import React from "react";
+export default function Nav() {
+  return (
+    <div className="nav_element">
+      <div>
+        <img src="/Sucheta.jpg" className="profile-picture" alt="potrait" />
+      </div>
+
+      <h4 className="subject-name">Sucheta Mukherjee</h4>
+      <h6 className="subject-title">Frontend Developer</h6>
+      <p className="subject-website">mukherjeesucheta.website</p>
+    </div>
+  );
 }
